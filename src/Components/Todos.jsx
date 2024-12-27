aa6f8d81ef0eec8928ae487a893b4d54e5b11daa
poison <shahsusan291@gmail.com>
2024-12-27 20:44:00 +0545

    Fix Auth

--- src/Components/Todos.jsx
+++ src/Components/Todos.jsx
@@ -1,56 +1,69 @@
-import React, { useEffect } from 'react'
-import 'react-toastify/dist/ReactToastify.css'
-import { toast, ToastContainer } from 'react-toastify'
-import { useState } from 'react'
-import { FaTrash } from 'react-icons/fa'
+import React, { useEffect } from "react";
+import "react-toastify/dist/ReactToastify.css";
+import { toast, ToastContainer } from "react-toastify";
+import { useState } from "react";
+import { FaTrash } from "react-icons/fa";
+import { useUser } from "@clerk/clerk-react";
+import { db } from "../Firebase/firebase";
+import { addDoc, collection, getDocs, deleteDoc, doc, query, where  } from "firebase/firestore";
 
 export default function Todos() {
-  const [input, setinput] = useState('')
-  const [todo, settodo] = useState([])
+  const [input, setinput] = useState("");
+  const [todo, settodo] = useState([]);
+
+  const {user} = useUser()
+  const userId = user?.id
+  console.log(userId)
 
   // Reading Data from Local Storage
-  useEffect(() => {
-    const savedData= localStorage.getItem('todos')
-    if(savedData){
-      settodo(JSON.parse(savedData))
-    }
-  }, [])
-
-useEffect(() => {
-  // Saving in Local Storage
-  localStorage.setItem('todos', JSON.stringify(todo))
-}, [todo])
-
-
-
 
   // Todo Add Function
 
-  const addTodo = () => {
+  const addTodo = async () => {
     if (input.length > 0 && input.length <= 40) {
-      const newTodo = {
-        title: input,
-        time: Date.now(),
-        completed: false
+      try {
+        const todoCollection = collection(db, "todos");
+
+        await addDoc(todoCollection, {
+          userId,
+          title: input,
+          time: Date.now(),
+          completed: false,
+        });
+
+        settodo([...todo, {userId,  title: input, time: Date.now(), completed: false }]);
+        setinput("");
+      } catch (error) {
+        console.log(error.message);
       }
-      settodo([...todo, newTodo]);
-      console.log(newTodo)
-      setinput('')
-    }
-    else {
+    } else {
       if (input.length === 0) {
-        toast.error('Todo Cannot Be Empty')
-      }
-      else if (input.length > 40) {
-        toast.error('Todo Must be Under 40 charcters Long!')
+        toast.error("Todo Cannot Be Empty");
+      } else if (input.length > 40) {
+        toast.error("Todo Must be Under 40 charcters Long!");
       }
     }
-  }
+  };
 
+  useEffect(() => {
+    const fetchTodos = async () => {
+      const todoquery = query(collection(db, "todos"), where("userId", "==", userId));
+      const querySnapshot = await getDocs(todoquery);
+      const todos = querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+      settodo(todos);
+    };
+    fetchTodos();
+  });
+  
 
-  const deleteTodo = () => {
-
-  }
+  const deleteTodo = async(id) => {
+      try {
+        await deleteDoc(doc(db, "todos", id));
+        settodo(todo.filter((item) => item.id !== id));
+      } catch (error) {
+        console.log(error.message)
+      }
+  };
 
   return (
     <>
@@ -66,34 +79,35 @@ useEffect(() => {
           />
           <button
             onClick={addTodo}
-            className='bg-purple-600 py-4 px-5 rounded-tr-lg rounded-br-lg'>Add</button>
+            className="bg-purple-600 py-4 px-5 rounded-tr-lg rounded-br-lg"
+          >
+            Add
+          </button>
         </div>
 
         {/* Todo Listing section */}
 
-        {
-          todo.map((todo, index) => {
-            return (
-              <div key={index} className="flex flex-row items-center bg-teal-600 rounded-lg mt-4 p-4 shadow-lg hover:bg-teal-800 transition duration-300 ease-in-out mx-64">
-                <h1 className="text-white font-mono text-lg flex-grow">{todo.title}</h1>
+        {todo.map((todo, index) => {
+          return (
+            <div
+              key={index}
+              className="flex flex-row items-center bg-teal-600 rounded-lg mt-4 p-4 shadow-lg hover:bg-teal-800 transition duration-300 ease-in-out mx-64"
+            >
+              <h1 className="text-white font-mono text-lg flex-grow">
+                {todo.title}
+              </h1>
               <button
-                onClick={() => deleteTodo(index)}
+                onClick={() => deleteTodo(todo.id)}
                 className="text-white p-2 rounded-full hover:bg-red-600 transition duration-200"
               >
-                <FaTrash /> 
+                <FaTrash />
               </button>
             </div>
-            
-            )
-          })
-        }
-
+          );
+        })}
       </div>
-
-
-
 
       <ToastContainer />
     </>
-  )
+  );
 }
